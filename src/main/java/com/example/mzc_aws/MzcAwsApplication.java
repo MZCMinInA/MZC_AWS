079350cd5d8a7eff66bb6a3f7d0c48d9b972701a
Mindun <pmk10077@gmail.com>
2025-11-07 16:04:04 +0900

    first commit

---
+++ src/main/java/com/example/mzc_aws/MzcAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mzc_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzcAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MzcAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mzc_aws/MzcAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mzc_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MzcAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

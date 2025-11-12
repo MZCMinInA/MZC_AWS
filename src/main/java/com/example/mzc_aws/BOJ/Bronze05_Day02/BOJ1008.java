e3ffbebac2e3724b3655f08b39b2c66876d24a0c
Mindun <pmk10077@gmail.com>
2025-11-12 17:03:48 +0900

    new java Coding Test [Bronze05]

---
+++ src/main/java/com/example/mzc_aws/BOJ/Bronze05_Day02/BOJ1008.java
@@ -0,0 +1,17 @@
+// 1008. A/B
+package com.example.mzc_aws.BOJ.Bronze05_Day02;
+
+import java.util.Scanner;
+
+public class BOJ1008 {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+
+        System.out.println((double)A/B);
+
+    }
+}
